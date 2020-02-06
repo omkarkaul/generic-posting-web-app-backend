620a8b4cd2f7818056f56cbf105210779b66278c
Omkar Kaul <omkar@xtra.co.nz>
2020-02-06 14:37:21 +1300

    added routes and logic for add_new_post and add_new_comment endpoints, also modified get_comments_for_post endpoint

--- controller.py
+++ controller.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, make_response, Response, jsonify
+from flask import Flask, request, make_response, jsonify
 from database import Database
 
 app = Flask(__name__)
 db = Database()
-
 
 @app.route("/api/posts", methods = ['GET'])
 def get_all_posts():
@@ -15,15 +14,14 @@ def get_all_posts():
     return response
 
 @app.route("/api/comments", methods = ['GET'])
-def get_comments_for_post(post):
+def get_comments_for_post():
     args = dict(request.args)
-
+    print(args)
     if 'post' in args:
         post = args['post']
-        print(post)
         
         comments = db.get_comments_for_post(post)
-
+        
         response = make_response(jsonify(comments), 200)
         response.headers['Content-Type'] = 'application/json'
 
@@ -31,16 +29,46 @@ def get_comments_for_post(post):
     else:
         return make_response(("Missing 'post' param!", 400))
 
-# @app.route("/api/posts", methods = ['POST'])
-# def add_post():
-#     args = dict(request.args)
+@app.route("/api/posts", methods = ['POST'])
+def add_new_post():
+    if request.is_json:
+        body = request.get_json()
 
-#     if 'name' in args:
-#         name = args['name']
+        post = body.get('post')
+
+        if post is None:
+            return make_response(('Bad request!', 400))
+
+        result = db.add_new_post(post)
         
-#         return app.make_response((f"Successfully added {name} into the DB!"), 200)
-#     else:
-#         return app.make_response(("Missing 'name' param!", 400))
+        if result == 1:
+            return make_response((f"Successfully posted {post.upper()} to the database!", 200))
+        elif result == 2:
+            return make_response((f"{post.upper()} is a duplicate entry!", 409))
+
+    return make_response(('Bad request!', 400))
+
+@app.route("/api/comments", methods = ['POST'])
+def add_new_comment():
+    args = dict(request.args)
+
+    if request.is_json and 'post' in args:
+        body = request.get_json()
+        
+        comment = body.get('comment')
+        post = args.get('post')
+    
+        if comment is None:
+            return make_response(('Bad request!', 400))
+
+        result = db.add_new_comment(post, comment)
+
+        if result == 1:
+            return make_response((f"Successfully added comment for post {post}!", 200))
+        elif result == 2:
+            return make_response((f"Post '{post}' doesn't exist!", 400))
+    
+    return make_response(("Bad data!", 400))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
